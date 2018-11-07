f0898d4e9074efab8c6a2e1d5f3fd6b8dca2afc2
SylvainJunca <sylvain.junca@gmail.com>
2018-11-07 15:24:53 -0500

    the app loads the tweets from the submit form - validates if the tweet is the correct length before

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,59 @@
 $( document ).ready(function() {
+
  
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+
+$(function() {
+  const $button = $('.new-tweet input');
+  $button.on('click', function (event) {
+    event.preventDefault();
+    if($('textarea').val().length > 140) {
+      alert('your message is too long');
+    } else if ($('textarea').val().length == 0){
+      alert('You should enter the thoughts you want to tweet');
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $('textarea').serialize(),
+        complete: loadTweets
+    })
+    }
+    
+  });
+});
+ 
+
+  const loadTweets = () => {
+    $.get('/tweets', function (data) {
+      $('#tweets-container').empty(); //empties the container to not charge/render tweets multiple times
+      renderTweets(data);
+    })
   }
-];
 
-const renderTweets = (tweets) => {
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
   }
-}
 
-const createTweetElement = (data) => {  
-  const $article = $("<article>").addClass("tweet");
-  const content =
-          `
-          <header class="tweet-header">
-            <img class="avatar" src=${data.user.avatars.small}>
-            <span class="full-name">${data.user.name}</span>
-            <span class="username">${data.user.handle}</span>
-          </header>
-          <div class="tweet-text">${data.content.text}</div>
-          <footer class="timestamp">
-            <div>${data.created_at}</div>
-          </footer>`;
+  const createTweetElement = (data) => {  
+    const $article = $("<article>").addClass("tweet");
+    const content =
+            `
+            <header class="tweet-header">
+              <img class="avatar" src=${data.user.avatars.small}>
+              <span class="full-name">${data.user.name}</span>
+              <span class="username">${data.user.handle}</span>
+            </header>
+            <div class="tweet-text">${data.content.text}</div>
+            <footer class="timestamp">
+              <div>${data.created_at}</div>
+            </footer>`;
 
-  return $article.append(content);
-}      
-renderTweets(data);
+    return $article.append(content);
+  }    
+  
+  loadTweets(); //renders the tweets when the page loads 
+  //renderTweets(data);
 });
