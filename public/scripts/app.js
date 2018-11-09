7a51b06917d30c9300c626e0aed4a10f7bfb3e92
SylvainJunca <sylvain.junca@gmail.com>
2018-11-09 17:16:48 -0500

    worked on the idendation of files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,28 @@
-
 // This function protects our tweets from Cross-Site Scripting
 function escape(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
-  const loadTweets = () => {
-    $.get('/tweets', function (data) {
-      $('#tweets-container').empty(); //empties the container to not charge/render tweets multiple times
-      $('textarea').val(''); 
-      renderTweets(data);
-    })
-  }
+const loadTweets = () => {
+  $.get('/tweets', function (data) {
+    $('#tweets-container').empty(); //empties the container to not charge/render tweets multiple times
+    $('textarea').val('');
+    renderTweets(data);
+  })
+}
 
-  const renderTweets = (tweets) => {
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $('#tweets-container').prepend($tweet);
-    }
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
   }
+}
 
-  const createTweetElement = (data) => {  
-    const $article = $("<article>").addClass("tweet");
-    const content =
-            `
+const createTweetElement = (data) => {
+  const $article = $("<article>").addClass("tweet");
+  const content =
+    `
             <header class="tweet-header">
               <img class="avatar" src=${data.user.avatars.small}>
               <span class="full-name">${data.user.name}</span>
@@ -39,45 +38,45 @@ function escape(str) {
               </span>
             </footer>`;
 
-    return $article.append(content);
-  }    
-  
-   //renders the tweets when the page loads 
-  //renderTweets(data);
+  return $article.append(content);
+}
+
+//renders the tweets when the page loads 
+//renderTweets(data);
 
 
-  $( document ).ready(function() {
+$(document).ready(function () {
 
- 
-    // The following toggles the new tweet area when the user
-    // clicks on the compose button. Not that by default, this
-    // element is hidden 
-    $( ".compose" ).on('click', (function() {
-    $( ".new-tweet" ).slideToggle( "fast" );
+
+  // The following toggles the new tweet area when the user
+  // clicks on the compose button. Not that by default, this
+  // element is hidden 
+  $(".compose").on('click', (function () {
+    $(".new-tweet").slideToggle("fast");
     $('textarea').focus();
-    }));
-  
-  
-  
-    
-    const $button = $('.new-tweet input');
-    $button.on('click', function (event) {
-      event.preventDefault();
-      if($('textarea').val().length > 140) {
-        $( ".error" ).html('Your message is too long to be posted').slideToggle( "slow" );
-      } else if ($('textarea').val().length == 0){
-        $( ".error" ).html('You should enter the thoughts you want to tweet').slideToggle( "slow" );
-      } else {
-        if ($(".error").is(":visible")) {
-          $(".error").slideToggle("slow");
-        }
-        $.ajax({
-          type: 'POST',
-          url: '/tweets',
-          data: $('textarea').serialize(),
-          complete: loadTweets
+  }));
+
+
+
+
+  const $button = $('.new-tweet input');
+  $button.on('click', function (event) {
+    event.preventDefault();
+    if ($('textarea').val().length > 140) {
+      $(".error").html('Your message is too long to be posted').slideToggle("slow");
+    } else if ($('textarea').val().length == 0) {
+      $(".error").html('You should enter the thoughts you want to tweet').slideToggle("slow");
+    } else {
+      if ($(".error").is(":visible")) {
+        $(".error").slideToggle("slow");
+      }
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $('textarea').serialize(),
+        complete: loadTweets
       })
-      };
-    });
-    loadTweets();
-  })
+    };
+  });
+  loadTweets();
+})
